b755d113eb77b838ee2fcf4f18b842eea8e12527
Adon <abdian.rizki@paper.id>
2024-03-15 00:23:45 +0700

    feat: compose use platform linux/amd64 & add more integration on astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-
 import tailwind from "@astrojs/tailwind";
+import compress from "astro-compress";
+import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
-  integrations: [mdx(), sitemap(), tailwind()]
+  integrations: [mdx(), sitemap(), tailwind(), compress(), robotsTxt()]
 });
